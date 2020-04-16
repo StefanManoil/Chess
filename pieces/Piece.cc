34c9a6ad3cdf7b386a5703c0a8e0facc7c635983
Sakin Haider <s26haide@uwaterloo.ca>
2020-04-16 14:30:11 -0400

    Piece classes without castling, en passant, and board->checkPieceSide()

--- pieces/Piece.cc
+++ pieces/Piece.cc
@@ -1,5 +1,145 @@
 #include "Piece.h"
 
+bool Piece::validCoordinate(std::pair<int,int> move) {
+    if (move.first < 0 || move.first > 7) {
+        return false;
+    }
+    else if (move.second < 0 || move.second > 7) {
+        return false;
+    }
+    return true;
+}
+
+std::vector<std::pair<int,int>> Piece::getStraightMoves() {
+    std::vector<std::pair<int,int>> moves;
+    std::pair<int,int> move;
+
+    int x = this->coordinates.first;
+    int y = this->coordinates.second;
+
+    //up straight
+    move = std::make_pair(x, y-1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first, move.second-1);
+        }
+    }
+
+    //down straight
+    move = std::make_pair(x, y+1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first, move.second+1);
+        }
+    }
+
+    //right straight
+    move = std::make_pair(x+1, y);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first+1, move.second);
+        }
+    }
+
+    //left straight
+    move = std::make_pair(x-1, y);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first-1, move.second);
+        }
+    }
+
+    return moves;
+}
+
+std::vector<std::pair<int,int>> Piece::getDiagonalMoves() {
+    std::vector<std::pair<int,int>> moves;
+    std::pair<int,int> move;
+
+    int x = this->coordinates.first;
+    int y = this->coordinates.second;
+
+    //up right
+    move = std::make_pair(x+1, y-1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first+1, move.second-1);
+        }
+    }
+
+    //up left
+    move = std::make_pair(x-1, y-1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first-1, move.second-1);
+        }
+    }
+
+    //down right
+    move = std::make_pair(x+1, y+1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first-1, move.second-1);
+        }
+    }
+
+    //down left
+    move = std::make_pair(x-1, y+1);
+    while(true) {
+        if (validCoordinate(move)) {
+            //if space is other colour
+            moves.emplace_back(move);
+            break;
+        
+            //if space is empty
+            moves.emplace_back(move);
+            move = std::make_pair(move.first-1, move.second+1);
+        }
+    }
+
+    return moves;
+}
+
 std::string Piece::toString() {
     Piecetype p = this->getType();
     if (this->side == Side::White) {
